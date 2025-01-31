77bf94da997413eb22d465155c9ee39dd150f65f
Ekaterina <katena.ponomareva.01@inbox.ru>
2025-02-01 03:39:52 +0700

    сумма счета

--- Sotik/src/Main.java
+++ Sotik/src/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+        int x = 200; //начальная сумма на счету клиента
+        int y = 1300; //сумма пополнение сета
+        int bonus;
+
+
+        if (y > 1000) {
+            bonus = y / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(x + y + bonus); //итоговая сумма на счету клиента
+    }
 }
